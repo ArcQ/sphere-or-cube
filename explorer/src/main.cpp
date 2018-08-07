bb0de65ee25d9eb90ec96c2e5dfe1ca0752b98f1
ArcQ <eddielaw296@gmail.com>
2018-08-06 23:15:05 -0400

    feat: fix up issues brought up by directory structure

--- explorer/src/main.cpp
+++ explorer/src/main.cpp
@@ -3,10 +3,9 @@ using namespace std;
 
 int main() 
 {
-
   char byte_arr[12] = {0};
   for (int i = 0; i < 12; ++i) {
-    cout << byte_arr[i] << " ";
+    cout << byte_arr[i] << "0";
   }
   cout << "Hello, World!";
   return 0;
