342198a547625edae3925f3a68a46ff5a2651cb3
eltonlaw <eltonlaw296@gmail.com>
2018-09-16 18:52:24 -0400

    change direction when bumped
    Event loop watches for spike in voltage from analog input (piezo sensor) and toggles the digital out sent to the l289n motor driver to reverse GND and VCC, changing the direction of the motor

--- firmware/README.md
+++ firmware/README.md
@@ -13,3 +13,7 @@ Libraries required to cross compile:
 Optional, an Arduino UNO was used as the hardware programmer.
 
     sudo pacman -S arduino
+
+## Notes
+
+The Makefile assumes that the first USB device you plug in is the programmer. For a quick hack, unplug everything, unplug everything, plug in the programmer first and then plug everything else back in.
--- firmware/blinkLED.c
+++ firmware/blinkLED.c
@@ -1,24 +1,28 @@
+// #ifndef F_CPU
+// #define F_CPU 20000000UL //20 Mhz Clock
+// #endif 
+
 #include <avr/io.h>
-#include <util.delay,h>
+#include <util/delay.h>
 
 int main(void) {
     // -------- Init -------------
     
     // Data Direction Register B, the 1 enables output
-    DDRB = 0b00000010;
+    DDRB |= 1 << PINB1;
 
     // -------- Event Loop -------
     while (1) {
-        PORTB = 0b00000010;
+        PORTB |= 1 << PINB1;
         _delay_ms(1000);
-        PORTB = 0b00000000;
+        PORTB &= ~(1 << PINB1);
         _delay_ms(1000);
 
         int i = 0;
         for (i=0; i<2; i++) {
-            PORTB = 0b00000010;
+            PORTB |= 1 << PINB1;
             _delay_ms(100);
-            PORTB = 0b00000000;
+        PORTB &= ~(1 << PINB1);
             _delay_ms(100);
         }
     }
